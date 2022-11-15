27eb92b148943952ac29c602cf91860dcd419bcd
GDJ56 <GDJ56@DESKTOP-MRG810C>
2022-11-15 10:19:14 +0900

    1114 부녀회장이 될테야 런타임 에러 못고침

--- PS_bxxxdx/src/com/bxxxdx/backjoon/Practice_2775.java
+++ PS_bxxxdx/src/com/bxxxdx/backjoon/Practice_2775.java
@@ -5,36 +5,28 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Practice_2775 {
-	//런타임에러
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader testCase = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(testCase.readLine());
-		String result = "";
-		for (int i = 0; i < N; i++) {
-			BufferedReader floor = new BufferedReader(new InputStreamReader(System.in));
-			int k = Integer.parseInt(floor.readLine());
+	//런타임에러 .. 진짜 왜 안되는지 몰겠다..
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		StringBuffer sb = new StringBuffer();
 
-			BufferedReader room = new BufferedReader(new InputStreamReader(System.in));
-			int n = Integer.parseInt(room.readLine());
-
-			int[][] arr = new int[k + 1][n];
-			for (int a = 0; a < k + 1; a++) {
-				for (int b = 0; b < n; b++) {
-					if(a==0) {
-						arr[a][b] = b+1;
-					}
-					else {
-						arr[a][b] = 0;
-						for(int c = 0; c <= b ; c ++ ) {
-							arr[a][b] += arr[a-1][c];
-						}
-					}
-//					System.out.print(arr[a][b] + " " );
-				}
-			}
-//			System.out.println(arr[k][n-1]);
-			result += arr[k][n-1] + "\n";
+		int[][] arr = new int[14][14];
+		for(int i=0;i<14;i++) {
+			arr[i][0] = 1;
+			arr[0][i] = i+1;
 		}
-		System.out.println(result);
+		for (int i = 1; i < 14; i++) {
+			for (int j = 1; j < 14; j++) {
+				arr[i][j] = arr[i-1][j] + arr[i][j-1];
+			}
+		}
+		
+		for(int i=0;i<N;i++) {
+			int k = Integer.parseInt(br.readLine());
+			int n = Integer.parseInt(br.readLine());			
+			sb.append(arr[k][n-1] + "\n");
+		}
+		System.out.println(sb);		
 	}
 }
