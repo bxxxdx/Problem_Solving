b97559b76d87330cf59c9713c156fb90518bca19
bxxxdx <qudeh911@gmail.com>
2023-01-10 09:22:46 +0900

    2023-01-10 [BD] 세탁소 사장 동혁 [B3]

---
+++ PS_bxxxdx/src/com/bxxxdx/backjoon/Practice_10817.java
@@ -0,0 +1,19 @@
+package com.bxxxdx.backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Practice_10817 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] temp = br.readLine().split(" ");
+		int[] arr = new int[temp.length];
+		for(int i=0;i<arr.length;i++) {
+			arr[i] = Integer.parseInt(temp[0]);
+		}
+		
+		
+		
+	}
+}
