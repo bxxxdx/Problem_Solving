70461c6862444c35ff24f46d5e87f41fffd10cd8
bxxxdx <qudeh911@gmail.com>
2023-02-20 09:41:21 +0900

    2023-02-20 [BD] 수 찾기 S4

---
+++ PS_bxxxdx/src/com/bxxxdx/backjoon/Practice_1920.java
@@ -0,0 +1,38 @@
+package com.bxxxdx.backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+//실패 - memory 초과
+public class Practice_1920 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		Arrays.sort(arr);
+
+		int M = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine(), " ");
+
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < M; i++) {
+			if(Arrays.binarySearch(arr, Integer.parseInt(st.nextToken())) >= 0) {
+				sb.append(1).append('\n');
+			} else {
+				sb.append(0).append('\n');
+			}
+		}
+		System.out.println(sb);
+	}
+}
