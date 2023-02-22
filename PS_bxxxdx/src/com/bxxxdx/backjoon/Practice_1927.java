e3b63ea813751683347a3ce2993cce06fd41678a
bxxxdx <qudeh911@gmail.com>
2023-02-22 16:06:01 +0900

    2023-02-22 [BD] 최소 힙 S2

---
+++ PS_bxxxdx/src/com/bxxxdx/backjoon/Practice_1927.java
@@ -0,0 +1,28 @@
+package com.bxxxdx.backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Practice_1927 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+		
+		PriorityQueue<Integer> pq = new PriorityQueue();
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < N; i++) {
+			String temp = br.readLine();
+			
+			if(temp.equals("0")) {
+				sb.append(pq.peek()!=null?pq.poll():"0").append('\n');
+			}
+			else {
+				pq.add(Integer.parseInt(temp));
+			}
+		}
+		System.out.println(sb);
+	}
+}
