430b234b7d3ace38000d9d96a81a735dfc9c363b
bxxxdx <qudeh911@gmail.com>
2023-02-23 09:09:39 +0900

    2023-02-23 [BD] 1로 만들기 S3

--- PS_bxxxdx/src/com/bxxxdx/backjoon/Practice_1463.java
+++ PS_bxxxdx/src/com/bxxxdx/backjoon/Practice_1463.java
@@ -3,40 +3,56 @@ package com.bxxxdx.backjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Practice_1463 {
+	static Integer[] arr;
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		int temp1 = N;
-		int temp2 = N;
-		int count1 = 0;
-		int count2 = 0;
-		while(temp1!=1) {
-			if(temp1%3 != 0){
-				temp1--;
-			}
-			else if(temp1%3 == 0){
-				temp1 = temp1/3;
-			}
-			else if(N%2 == 0){
-				temp1 = temp1/2;
-			}
-			count1++;
-		}
-		while(temp2!=1) {
-			if(temp2%2 != 0){
-				temp2--;
-			}
-			else if(temp2%2 == 0){
-				temp2 = temp2/2;
-			}
-			else if(temp2%3 == 0){
-				temp2 = temp2/3;
-			}
-			count2++;
-		}
-		System.out.println(count1<count2?count1:count2);
+		arr = new Integer[N + 1];
+		arr[0] = arr[1] = 0;
+		System.out.println(solve(N));
 	}
+
+	static int solve(int n) {
+		if (arr[n] == null) {
+			if (n % 6 == 0) {
+				arr[n] = Math.min(solve(n - 1), Math.min(solve(n / 3), solve(n / 2))) + 1;
+			} else if (n % 3 == 0) {
+				arr[n] = Math.min(solve(n / 3), solve(n - 1)) + 1;
+			} else if (n % 2 == 0) {
+				arr[n] = Math.min(solve(n / 2), solve(n - 1)) + 1;
+			} else {
+				arr[n] = solve(n - 1) + 1;
+			}
+		}
+		return arr[n];
+	}
+
+//	public static int dp(int n, int[] arr) {
+//		switch (n) {
+//		case 1:
+//			arr[n] = 0;
+//			break;
+//		case 2:
+//		case 3:
+//			arr[n] = 1;
+//			break;
+//		default:
+//			int a=Integer.MAX_VALUE; int b=Integer.MAX_VALUE; int c=Integer.MAX_VALUE;
+//			if (n % 3 == 0) {
+//				a = dp(n / 3, arr) + 1;
+//			}
+//			if (n % 2 == 0) {
+//				b = dp(n / 2, arr) + 1;
+//			}
+//			c = dp(n - 1, arr) + 1;
+//			
+//			arr[n] = Math.min(a, Math.min(b, c));
+//			break;
+//		}
+//		return arr[n];
+//	}
 }
