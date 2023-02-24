3c1cb52b7e8f61fcb4853f1a90f5fdb2cd71c4b3
bxxxdx <qudeh911@gmail.com>
2023-02-24 11:10:32 +0900

    2023-02-24 [BD] 돌 게임 S5

---
+++ PS_bxxxdx/src/com/bxxxdx/backjoon/Practice_9655.java
@@ -0,0 +1,15 @@
+package com.bxxxdx.backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Practice_9655 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		System.out.println(N%2==0?"CY":"SK");
+	}
+}
