5d8cbd62c3762c99780fbc4d2047e2d14e826ad8
bxxxdx <qudeh911@gmail.com>
2023-03-01 10:50:57 +0900

    2023-03-01 [BD] 이친수 S3 <dp문제>

---
+++ PS_bxxxdx/src/com/bxxxdx/backjoon/Practice_2193.java
@@ -0,0 +1,21 @@
+package com.bxxxdx.backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Practice_2193 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		long[] dp = new long[91];
+		dp[1] = 1;
+		dp[2] = 1;
+		for(int i=3;i<91;i++) {
+			dp[i] = dp[i-1] + dp[i-2];
+		}
+		System.out.println(dp[N]);
+		
+	}
+}
