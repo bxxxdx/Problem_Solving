26dc3d7ec6b1f3ab2371ccd3789608a942d71fc2
bxxxdx <qudeh911@gmail.com>
2023-03-02 10:59:25 +0900

    2023-03-02 [BD] 제곱수의 합 S2 <dp>

---
+++ PS_bxxxdx/src/com/bxxxdx/backjoon/Practice_1699.java
@@ -0,0 +1,25 @@
+package com.bxxxdx.backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Practice_1699 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+
+		int[] dp = new int[n + 1];
+
+		dp[1] = 1;
+		for (int i = 2; i <= n; i++) {
+			dp[i] = 5;
+		}
+		for (int i = 2; i <= n; i++) {
+			for (int j = 1; j * j <= i; j++) {
+				dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+			}
+		}
+		System.out.println(dp[n]);
+	}
+}
