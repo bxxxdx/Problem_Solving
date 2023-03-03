af4d7187acf93deca8c594ace0351e37499f5daf
bxxxdx <qudeh911@gmail.com>
2023-03-03 12:31:18 +0900

    2023-03-03 [BD] 이동하기 S2 <dp>

--- PS_bxxxdx/src/com/bxxxdx/backjoon/Practice_1965.java
+++ PS_bxxxdx/src/com/bxxxdx/backjoon/Practice_1965.java
@@ -3,7 +3,6 @@ package com.bxxxdx.backjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Practice_1965 {
