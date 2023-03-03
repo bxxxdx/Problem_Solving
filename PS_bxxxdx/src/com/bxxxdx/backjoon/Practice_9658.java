d9a60749d65f49dc5ef599af16390287071cdfd8
bxxxdx <qudeh911@gmail.com>
2023-03-03 10:57:48 +0900

    2023-03-03 [BD] 돌 게임4 S2 <dp>

---
+++ PS_bxxxdx/src/com/bxxxdx/backjoon/Practice_9658.java
@@ -0,0 +1,22 @@
+package com.bxxxdx.backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Practice_9658 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+
+		if (N % 7 == 1 || N % 7 == 3) {
+			System.out.println("CY");
+		} else {
+			System.out.println("SK");
+		}
+
+	}
+
+}
