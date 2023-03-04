381199d13940d4b6578deefcaeddd7f03a1c1188
bxxxdx <qudeh911@gmail.com>
2023-03-04 17:40:30 +0900

    2023-03-04 [BD] 자원 캐기 S2 <dp>

---
+++ PS_bxxxdx/src/com/bxxxdx/backjoon/Practice_14430.java
@@ -0,0 +1,44 @@
+package com.bxxxdx.backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Practice_14430 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] temp = br.readLine().split(" ");
+
+		int N = Integer.parseInt(temp[0]);
+		int M = Integer.parseInt(temp[1]);
+
+		int[][] arr = new int[N][M];
+		StringTokenizer st;
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < M; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+
+		int[][] dp = new int[300][300];
+		dp[0][0] = arr[0][0];
+		for (int i = 1; i < N; i++) {
+			dp[i][0] = dp[i - 1][0] + arr[i][0];
+		}
+		for (int i = 1; i < M; i++) {
+			dp[0][i] = dp[0][i - 1] + arr[0][i];
+		}
+		for (int i = 1; i < N; i++) {
+			for (int j = 1; j < M; j++) {
+				dp[i][j] = Math.max(dp[i-1][j] + arr[i][j], dp[i][j-1] + arr[i][j]);
+			}
+		}
+//		for (int i = 0; i < N; i++) {
+//			System.out.println(Arrays.toString(dp[i]));
+//		}
+		System.out.println(dp[N-1][M-1]);
+	}
+}
