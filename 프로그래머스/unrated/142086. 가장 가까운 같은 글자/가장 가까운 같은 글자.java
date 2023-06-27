d9af0a0d63becfeb7544c412c3127980afbf5ea4
Byoungdo Lee <33855575+bxxxdx@users.noreply.github.com>
2023-06-27 09:17:54 +0900

    [unrated] Title: 가장 가까운 같은 글자, Time: 5.81 ms, Memory: 79.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] solution(String s) {
+        int[] answer = new int[s.length()];
+        for(int i=0;i<s.length();i++){
+            char temp = s.charAt(i);
+            int count = 1;
+            boolean flag = false;
+            for(int j=i-1;j>=0;j--){
+                char temp2 = s.charAt(j);
+                if(temp == temp2){
+                    answer[i] = count;
+                    flag = true;
+                    break;
+                }
+                count++;
+            }
+            if(!flag)
+                answer[i] = -1;
+        }
+        return answer;
+    }
+}
